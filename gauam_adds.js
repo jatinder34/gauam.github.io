d4bb7a48c3c18790774be3eb65f50a737158ae85
jatinder kumar <jatinderkumar0550@gmail.com>
2020-09-11 21:31:39 +0530

    Create gauam_adds.js

---
+++ gauam_adds.js
@@ -0,0 +1,6 @@
+document.getElementById('gauam_add_5555553333').innerHTML = '<div id="gmyModal_add_5555553333" class="modal_add_5555553333"><div class="modal-content_add_5555553333"><img width="500" height="500" src="https://otech.guam.gov/wp-otech-content/themes/Divi-child/images/Guam-Covid-Alert_Social-Media.png"><span class="close_add_5555553333">&times;</span></div></div></div><style>body{font-family:Arial,Helvetica,sans-serif}.modal_add_5555553333{display:block;position:fixed;z-index:1;padding-top:100px;left:0;top:0;width:100%;height:100%;overflow:auto;background-color:#000;background-color:rgba(0,0,0,.4)}.modal-content_add_5555553333{position: absolute;top: 16px; right: 27%;background-color:#fefefe;margin:auto;padding:20px;border:1px solid #888;width:37%}.close_add_5555553333{color:#aaa;float:right;font-size:28px;font-weight:700}.close_add_5555553333:focus,.close_add_5555553333:hover{color:#000;text-decoration:none;cursor:pointer}</style>';
+var span = document.getElementsByClassName("close_add_5555553333")[0];
+span.onclick = function() {
+    alert('ddd')
+    document.getElementById("gmyModal_add_5555553333").style.display = "none";
+}
